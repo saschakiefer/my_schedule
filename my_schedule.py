8b26cdbaf6161010ba18faf9c352ccc6b9147bdc
Sascha Kiefer <mail@sascha-kiefer.de>
2020-11-11 17:55:25 +0100

    Update output for obsidian

--- my_schedule.py
+++ my_schedule.py
@@ -56,7 +56,7 @@ class Calendar(ApiComponent):
             # I don't need myself in the participants list
             if event["organizer"]["emailAddress"]["address"] != "sascha.kiefer@sap.com":
                 participants.append(
-                    "#[[" + event["organizer"]["emailAddress"]["name"] + "]]"
+                    "[[" + event["organizer"]["emailAddress"]["name"] + "]]"
                 )
 
             # Attendees
@@ -67,11 +67,11 @@ class Calendar(ApiComponent):
 
                     if (
                         email != "sascha.kiefer@sap.com"  # don't need me in the list
-                        and ("#[[" + name + "]]")
+                        and ("[[" + name + "]]")
                         not in participants  # filter the organizer from the attendees list
                         and name.startswith("DL ") is False  # Filter Delivery Lists
                     ):
-                        participants.append("#[[" + name + "]]")
+                        participants.append("[[" + name + "]]")
 
             my_day.append(
                 {
@@ -91,7 +91,8 @@ class Calendar(ApiComponent):
                 if len(event["participants"]) > 0
                 else ""
             )
-            md = f"* __{event['start_time'].strftime('%H:%M')}__ - [{event['subject']}]({event['link']}) {participants_string} #[[Meeting Minutes]]"
+            # md = f"* __{event['start_time'].strftime('%H:%M')}__ - [{event['subject']}]({event['link']}) {participants_string} #[[Meeting Minutes]]"
+            md = f"- _{event['start_time'].strftime('%H:%M')}_ - {event['subject']} {participants_string} [[Meeting Minutes]]"
             md = md.replace("  ", " ")
 
             click.echo(md)
